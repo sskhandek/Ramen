317c326213e26d5e03cd92ec2e4dcbea3c46cea1
sskhandek <sskhandek@gmail.com>
2013-10-24 16:55:49 -0500

    Updated Storyboard and organized

---
+++ Ramen/Ramen/ViewControllers/LoginSequence/SSKLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSKLoginViewController.h
+//  Ramen
+//
+//  Created by Sujay Khandekar on 10/24/13.
+//  Copyright (c) 2013 Sujay Khandekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSKLoginViewController : UIViewController
+
+@end
---
+++ Ramen/Ramen/ViewControllers/QuickListsSequence/SSKQuickListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSKQuickListTableViewController.h
+//  Ramen
+//
+//  Created by Sujay Khandekar on 10/24/13.
+//  Copyright (c) 2013 Sujay Khandekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSKQuickListTableViewController : UITableViewController
+
+@end
